d311b511c4997c01c42ae007303a65995705143f
Beatriz <beatrizleite_aragao@hotmail.com>
2023-10-08 11:46:25 -0300

    auth-token

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -1,8 +1,8 @@
 import json
 
+from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.test import APITestCase , APIClient
-
 from django.urls import reverse
 
 from product.factories import CategoryFactory ,  ProductFactory
@@ -17,21 +17,35 @@ class TestOrderViewSet(APITestCase):
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title = 'mouse' , price =100 , category = [self.category])
         self.order = OrderFactory(product = [self.product])
+
+        #Criando um usuário e pegando a autentificação
+        self.user = UserFactory()
+        token = Token.objects.create(user = self.user)
+        token.save()
+
     def test_order(self):
+        token = Token.objects.get(user__username = self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION = "Token " + token.key)
+
         response = self.client.get(
             reverse('order-list' , kwargs = {'version':'v1'})
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        order_data = json.loads(response.content)[0]
-        self.assertEqual(order_data['product'][0]['title'] , self.product.title) 
-        self.assertEqual(order_data['product'][0]['price'] , self.product.price)
-        self.assertEqual(order_data['product'][0]['active'] , self.product.active)
-        self.assertEqual(order_data['product'][0]['category'][0]['title'] , self.category.title)   
+        #import pdb; pdb.set_trace()
+        #mudança para se adequar a paginação
+        order_data = json.loads(response.content)
+        self.assertEqual(order_data["results"][0]['product'][0]['title'] , self.product.title) 
+        self.assertEqual(order_data["results"][0]['product'][0]['price'] , self.product.price)
+        self.assertEqual(order_data["results"][0]['product'][0]['active'] , self.product.active)
+        self.assertEqual(order_data["results"][0]['product'][0]['category'][0]['title'] , self.category.title)   
     
     def test_create_order(self):
         user = UserFactory()
+        token = Token.objects.get(user__username = self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION = "Token " +token.key)
+
         product = ProductFactory()
+        
         data = json.dumps({
             'products_id' : [product.id] ,
             'user' : user.id
--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,8 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer #requisito
-    queryset = Order.objects.all() #requisito , padrão para o viewset 
+    queryset = Order.objects.all().order_by("id") #requisito , padrão para o viewset 
+
+    authentication_classes = [TokenAuthentication]# add Token authentication
+    permission_classes = [IsAuthenticated]
+
